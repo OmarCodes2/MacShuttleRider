26d580af56f7f5e91e1c8f885765669a1a9b647d
OmarCodes2 <omarcodes2@gmail.com>
2024-06-08 13:30:50 -0400

    add map feature

--- MacShuttleRider/App.js
+++ MacShuttleRider/App.js
@@ -1,11 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar barStyle="dark-content" />
+      <View style={styles.header}>
+        <Text style={styles.headerText}>mac shuttle</Text>
+      </View>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>Footer Text</Text>
+      </View>
     </View>
   );
 }
@@ -14,7 +30,30 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  header: {
+    backgroundColor: 'rgb(122, 0, 60)',
+    paddingTop: 40,
+    paddingBottom: 10,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  headerText: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  map: {
+    flex: 1,
+  },
+  footer: {
+    backgroundColor: 'rgb(253, 191, 87)',
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  footerText: {
+    color: '#fff',
+    fontSize: 16,
   },
 });
